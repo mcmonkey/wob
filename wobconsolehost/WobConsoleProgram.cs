abd331bbb3d4b35f23fbf9ce0ddba2bde6a9dd08
Jason Green <mcmonkeygames@gmail.com>
2012-04-30 13:24:09 -0400

    Added wob service library, xna library, a host console app for the svc, and a pc client for the xna lib.

---
+++ wobconsolehost/WobConsoleProgram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using wobsvclib;
+
+namespace wobconsolehost
+{
+    class WobConsoleProgram
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Uri baseAddress = new Uri("http://localhost:8000/wob/Login");
+            ServiceHost selfHost = null;
+            try
+            {
+                selfHost = new ServiceHost(typeof(LoginService), baseAddress);
+                selfHost.AddServiceEndpoint(typeof(ILogin), new WSDualHttpBinding(), "LoginService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("Open");
+                while (true)
+                {
+                    command = Console.ReadLine();
+                    if (command == "e")
+                        break;
+                }
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("Failure!");
+                Console.WriteLine(ce);
+                Console.ReadLine();
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ wobsvclib/ILogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wobsvclib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace="https://Wob.Services")]
+    public interface ILogin
+    {
+        [OperationContract]
+        bool Login(string username, string password = null);
+    }
+}
